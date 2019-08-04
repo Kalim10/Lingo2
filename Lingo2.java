31bf34c0425db0e189ba1ce3b0abb366aa00d9cd
Kalim <kalim.r1989@gmail.com>
2019-08-04 12:37:17 +0200

    laatste wijziging

--- Lingo2.java
+++ Lingo2.java
@@ -8,47 +8,50 @@ public class Lingo2 {
     private String antwoord = GeheimWoord.randomWoord();
     private int counter = 0;
 
-    
 
     public boolean spelen() {
         System.out.println("Lingo");
+        System.out.println("1 = letter juist en positie juist");
+        System.out.println("3 = letter juist maar positie onjuist");
+        System.out.println("0 = letter onjuist en positie onjuist");
+        System.out.println("eerste letter staat al voor je klaar:");
         do {
             System.out.println();
             System.out.println(antwoord.charAt(0));
             poging = input.nextLine();
-            for (int x = 0; x < antwoord.length(); x++) {
-                int checken = checken(poging, x);
-                System.out.print(checken);
+            for (
+                    int x = 0; x < antwoord.length(); x++) {
+                    int checken = checken(poging, x);
+                    System.out.print(checken);
             }
 
             counter++;
         }
         while (
                 counter < 5 &&
-                !poging.equals(antwoord)
+                        !poging.equals(antwoord)
         );
 
-        if(counter == 5) {
+        if (counter == 5) {
             System.out.println("\nHelaas! ");
             return false;
-        }
-        else{
+        } else {
             System.out.println("\nGefeliciteerd!");
             return true;
         }
     }
 
     private int checken(String input, Integer antwoordIndex) {
-        final char answerLetter = antwoord.charAt(antwoordIndex);
+        final char gecheckteLetter = antwoord.charAt(antwoordIndex);
 
-        if(
+        if (
                 input.length() > antwoordIndex &&
-                input.charAt(antwoordIndex) == antwoord.charAt(antwoordIndex)
+                        input.charAt(antwoordIndex) == antwoord.charAt(antwoordIndex)
         ) {
             return 1;
         }
 
-        if(input.contains(String.valueOf(answerLetter))){
+        if (input.contains(String.valueOf(gecheckteLetter))) {
             return 3;
         }
 
