039a25aa012d699e96fa014863535f1efe31d089
Kalim <kalim.r1989@gmail.com>
2019-08-04 09:28:30 +0200

    1 en 0 werkt

--- Lingo2.java
+++ Lingo2.java
@@ -7,19 +7,20 @@ public class Lingo2 {
     String antwoord;
     Scanner input = new Scanner(System.in);
     GeheimWoord woord = new GeheimWoord();
+    //woord.randomWoord();
+    String antwoord1 = woord.randomWoord();
 
 
     public void spelen() {
         System.out.println("Lingo");
-        woord.randomWoord();
-        String antwoord = woord.randomWoord();
+
         do {
             System.out.println();
-            System.out.println(antwoord.charAt(0));
+            System.out.println(antwoord1.charAt(0));
             poging = input.nextLine();
             // System.out.println(poging);
             //System.out.println();
-            for(int x=0; x<antwoord.length();x++){
+            for (int x = 0; x < antwoord1.length(); x++) {
                 int checken = checken(poging.charAt(x));
                 //System.out.print(checken);
             }
@@ -28,12 +29,13 @@ public class Lingo2 {
         while (!poging.equals(antwoord));
     }
 
-    int checken(char letter) {
-        String antwoord = woord.randomWoord();
-        for (int x = 0; x < antwoord.length(); x++) {
-            if (letter == antwoord.charAt(x)) {
+    private int checken(char letter) {
+        //String antwoord = woord.randomWoord();
+        for (int x = 0; x < antwoord1.length(); x++) {
+            if (letter == antwoord1.charAt(x)) {
                 System.out.print("1");
                 return 1;
+                
             }
         }
         System.out.print("0");
